bb704b9172206bb1fbaa3e009d35c951ba4b302e
mmayers88 <mmayers@pdx.edu>
2020-05-17 14:55:25 -0700

    adding basic flow structure

---
+++ pipeLine.py
@@ -0,0 +1,9 @@
+
+
+def iORrORn():
+    #6 MSB are the opcode
+
+def JR(address):
+    jumpTo = address >> 4
+    jumpTo = jumpTo - 1
+    return jumpTo
---
+++ structure.py
@@ -0,0 +1,14 @@
+import queue
+
+fileName = "sample_memory_image.txt"
+
+memory = open(fileName, 'r')
+PC = 0
+lines = memory.readlines()
+#print(lines)
+
+
+while PC < len(lines):
+    print(lines[PC])
+    PC += 1
+print(len(lines))
