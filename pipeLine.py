e62e441119097c41d98c2d1bd8e1cb14e7fdfe6c
mmayers88 <mmayers@pdx.edu>
2020-05-18 13:15:25 -0700

    incremenet pipeline and using dictionary structure

--- pipeLine.py
+++ pipeLine.py
@@ -1,7 +1,15 @@
-import queue
-class pipeline:
+
+class CPU:
+    pipeline = {1:'x', 2: 'x', 3: 'x', 4: 'x', 5: 'x'}
+    PC = 0
     def __init__(self):
-        self.PC = 0
+        self.pipeline[1]='1'
+        return      
+
+    def printData(self):
+        print(self.pipeline)
+        print(self.PC)
+        return
 
     #instruction fetch
     def IF(self):
@@ -36,4 +44,34 @@ class pipeline:
 
     #this will be the "main" function basically
     def cycle(self):
+        #IF
+        #ID
+        #EX
+        #MEM
+        #WB
+        
+        #increment pipeline
+        self.pipeline[5] = self.pipeline[4]
+        self.pipeline[4] = self.pipeline[3]
+        self.pipeline[3] = self.pipeline[4]
+        self.pipeline[2] = self.pipeline[1]
+        self.pipeline[1] = 'x'
         return
+
+
+def main():
+    test = CPU
+    #print(test.printData(test))
+    test.pipeline[1] = '1'
+    test.pipeline[2] = '2'
+    test.pipeline[3] = '3'
+    test.pipeline[4] = '4'
+    test.pipeline[5] = '5'
+    print(test.pipeline)
+    test.cycle(test)
+    print(test.pipeline)
+    test.cycle(test)
+    print(test.pipeline)
+
+if __name__ == "__main__":
+    main()
