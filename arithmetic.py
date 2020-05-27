3145bc3de8842e85a349840843c8bb20d8a13fd5
a <asd>
2020-05-26 19:39:52 -0700

    Completed the rest of the arithmetic functions

---
+++ arithmetic.py
@@ -0,0 +1,15 @@
+    def SUB(self, RS, RT):
+        answer = RS - RT
+        return answer
+
+    def SUBI(self, RS, IMM):
+        answer = RS - IMM
+        return
+
+    def MUL(self, RS, RT):
+        answer = RS * RT
+        return answer
+
+    def MULI(self, RS, IMM):
+        answer = RS * IMM
+        return
